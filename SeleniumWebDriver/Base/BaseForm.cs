49d9d43e2d5cd376d76c2d0e77c305598f4f097c
ALI <ALI@DESKTOP-M6JB5O2>
2020-07-22 01:33:37 +0300

    Add project files.

---
+++ SeleniumWebDriver/Base/BaseForm.cs
@@ -0,0 +1,29 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SeleniumWebDriver.Base
+{
+ public   class BaseForm:Form
+    {
+     public   IWebDriver driver;
+        
+        public BaseForm()
+        {
+            var relativePath = @"C:\Users\ALI\source\repos\EdgeDriverTest\EdgeDriverTest\bin\Debug\netcoreapp2.1";
+            var options = new ChromeOptions();
+            options.AddArgument("--start-maximized");//full screen ekran açar
+            driver = new ChromeDriver(relativePath, options);
+
+        }
+        public void QuitSeleniumDriver()
+        {
+            driver.Quit();
+        }
+    }
+}
---
+++ SeleniumWebDriver/Form1.cs
@@ -0,0 +1,73 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using SeleniumWebDriver.Base;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SeleniumWebDriver
+{
+    public partial class Form1 : BaseForm
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            driver.Url = "https://aof.anadolu.edu.tr/anasayfa/Default.aspx";
+            
+            var element = driver.FindElement(By.Id("liOgrenci"));
+            element.FindElement(By.TagName("a")).Click();
+            driver.FindElement(By.Id("txtAccount")).SendKeys("12325613216");
+            driver.FindElement(By.Id("txtPassword")).SendKeys("78244818");
+            driver.FindElement(By.Id("doButton")).Click();
+            driver.Url = "https://aof.anadolu.edu.tr/ogrenci/DersDurum.aspx";
+            var table = driver.FindElements(By.ClassName("DTableSutun")).Where(x=>x.GetAttribute("id").Contains("pnlAciklama"));
+            string message=string.Empty ;
+            foreach (var durumItem in table.GroupBy(x=>x.Text).Select(x=>x.Key))
+            {
+                message+= $"{durumItem}: {GetDersCount(table, durumItem)}\n";
+            }
+
+            MessageBox.Show( message, "Ders Durum");
+        }
+        public int GetDersCount(IEnumerable<IWebElement> table,string whereClouse)
+        {
+            return table.Where(x => x.Text == whereClouse).Count();
+        }
+        public void Sahininden()
+        {
+            driver.Url = "https://secure.sahibinden.com/giris";
+            driver.FindElement(By.Id("username")).SendKeys("aliihsanakay");
+            driver.FindElement(By.Id("password")).SendKeys(".");
+            driver.FindElement(By.ClassName("show-hide-trigger")).Click();
+            driver.FindElement(By.Id("userLoginSubmitButton")).Click();
+
+            var loginControl = driver.FindElements(By.ClassName("error"));
+            if (loginControl.Any())
+            {
+                MessageBox.Show(loginControl.FirstOrDefault().Text, "Hata");
+            }
+            else//login olduk demek
+            {
+
+            }
+
+        }
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            QuitSeleniumDriver();
+        }
+
+       
+    }
+}
